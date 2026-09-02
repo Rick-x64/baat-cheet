06c3d4a1161924a59db44767f73b0f74bd258699
Rick-x64 <rinkeshp7171@gmail.com>
2026-09-01 21:44:32 -0500

    logout button

--- Frontend/src/pages/ChatPage.jsx
+++ Frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,14 @@
-
-
+import { useAuthStore } from "../store/useAuthStore";
+// import { useState } from "react";
 function ChatPage() {
-    return
-    <div>
-        Chat Page
-    </div>;
+    const { logout } = useAuthStore();
+    return (
+        <div className="z-10">
+            Chat Page
+            <button onClick={logout}>button</button>
+        </div>
+    );
 
 }
 
-export default ChatPage
+export default ChatPage;
