cefc83309a0ef606710a2124bfccb6e701ba15ff
Rick-x64 <rinkeshp7171@gmail.com>
2026-09-02 02:46:47 -0500

    chat page setup

---
+++ Frontend/src/components/ChatsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatsList() {
+    return (
+        <div>
+            chat list
+        </div>
+    )
+}
+
+export default ChatsList
---
+++ Frontend/src/components/NoConversationPlaceholder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoConversationPlaceholder() {
+    return (
+        <div>
+            no conv
+        </div>
+    )
+}
+
+export default NoConversationPlaceholder
