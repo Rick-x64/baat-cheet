58db440ec0cea12e5f97cd09a728f099face9ef3
Rick-x64 <rinkeshp7171@gmail.com>
2026-09-12 16:49:09 -0500

    socket realtime message imp.

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -20,6 +20,8 @@ const io = new Server(server, {
 // apply authentication middleware to all socket connections
 io.use(socketAuthMiddleware);
 
+
+
 // we will use this function to check if the user is online or not
 export function getReceiverSocketId(userId) {
     return userSocketMap[userId];
